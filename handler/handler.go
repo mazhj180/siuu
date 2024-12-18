28d99231bad4215fd5e51271b32ef25868099a59
mazhj <mazhj180@163.com>
2024-12-18 09:49:50 +0800

    :sparkles: add processing handler

---
+++ handler/handler.go
@@ -0,0 +1,43 @@
+package handler
+
+import (
+	"evil-gopher/proxy"
+	"evil-gopher/session"
+)
+
+var (
+	handlers []handle
+)
+
+func init() {
+	handlers = []handle{
+		loggingHandle,
+		proxyHandle,
+	}
+}
+
+type handle func(*context)
+
+type context struct {
+	session session.Session
+	index   int
+}
+
+func (c *context) next() {
+	c.index++
+	if c.index < len(handlers) {
+		handlers[c.index](c)
+	}
+}
+
+func (c *context) setProxy(p proxy.Proxy) {
+	c.session.SetProxy(p)
+}
+
+func Run(s session.Session) {
+	ctx := &context{
+		session: s,
+		index:   -1,
+	}
+	ctx.next()
+}

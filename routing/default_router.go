7b8a9a0787a933485c6fc378d4382f39eb2cbfbc
mazhj <mazhj180@163.com>
2024-12-29 01:17:05 +0800

    :recycle: refactor the code of the routing section

---
+++ routing/default_router.go
@@ -0,0 +1,143 @@
+package routing
+
+import (
+	"fmt"
+	"github.com/lionsoul2014/ip2region/binding/golang/xdb"
+	"github.com/spf13/viper"
+	"net"
+	"os"
+	"siu/server/store"
+	"siu/tunnel/proxy"
+	"strings"
+)
+
+type wildcard struct {
+	rule string
+	prx  proxy.Proxy
+}
+
+type routeTable struct {
+	exacts    map[string]proxy.Proxy
+	wildcards []*wildcard
+	geo       map[string]proxy.Proxy
+}
+
+type DefaultRouter struct {
+	route   routeTable
+	ipXdb   []byte
+	xdbPath string
+}
+
+func NewDefaultRouter(route, xdbp string) (*DefaultRouter, error) {
+	v := viper.New()
+	v.SetConfigFile(route)
+	if err := v.ReadInConfig(); err != nil {
+		return nil, err
+	}
+
+	r := &DefaultRouter{
+		route: routeTable{
+			exacts:    make(map[string]proxy.Proxy),
+			wildcards: make([]*wildcard, 0),
+			geo:       make(map[string]proxy.Proxy),
+		},
+	}
+
+	exacts := v.GetStringSlice("route.exacts")
+	for _, val := range exacts {
+		val = strings.TrimSpace(val)
+		kvs := strings.Split(val, ",")
+
+		prx := store.GetProxy(kvs[1])
+		if prx == nil {
+			_, _ = os.Stdout.WriteString(fmt.Sprintf("failed to get proxy: %s\n", kvs[1]))
+			continue
+		}
+		r.route.exacts[kvs[0]] = store.GetProxy(kvs[1])
+	}
+
+	wildcards := v.GetStringSlice("route.wildcards")
+	for _, val := range wildcards {
+		val = strings.TrimSpace(val)
+		kvs := strings.Split(val, ",")
+
+		prx := store.GetProxy(kvs[1])
+		if prx == nil {
+			_, _ = os.Stdout.WriteString(fmt.Sprintf("failed to get proxy: %s\n", kvs[1]))
+			continue
+		}
+		r.route.wildcards = append(r.route.wildcards, &wildcard{
+			rule: kvs[0],
+			prx:  prx,
+		})
+	}
+
+	geo := v.GetStringSlice("route.geo")
+	for _, val := range geo {
+		val = strings.TrimSpace(val)
+		kvs := strings.Split(val, ",")
+
+		prx := store.GetProxy(kvs[1])
+		if prx == nil {
+			_, _ = os.Stdout.WriteString(fmt.Sprintf("failed to get proxy: %s\n", kvs[1]))
+			continue
+		}
+		r.route.geo[kvs[0]] = prx
+	}
+	xdbb, err := xdb.LoadVectorIndexFromFile(xdbp)
+	if err != nil {
+		panic(fmt.Errorf("failed to load ip router please check or disable autorouting err: %s", err))
+	}
+	r.ipXdb = xdbb
+	r.xdbPath = xdbp
+	return r, nil
+}
+
+func (r *DefaultRouter) Route(host string) (proxy.Proxy, error) {
+
+	if p, ok := r.route.exacts[host]; ok {
+		return p, nil
+	}
+
+	for _, w := range r.route.wildcards {
+		rl := len(w.rule[1:])
+		hl := len(host)
+		if hl < rl {
+			continue
+		}
+		if host[hl-rl:] == w.rule[1:] {
+			return w.prx, nil
+		}
+	}
+
+	searcher, err := xdb.NewWithVectorIndex(r.xdbPath, r.ipXdb)
+	if err != nil {
+		return nil, fmt.Errorf("failed to load ip router please check or disable autorouting err: %s", err)
+	}
+
+	ips, err := net.LookupIP(host)
+	if err != nil || len(ips) == 0 {
+		return nil, fmt.Errorf("failed to lookup ip err: %s", err)
+	}
+	str, err := searcher.SearchByStr(ips[0].String())
+	if err != nil {
+		return nil, fmt.Errorf("failed to search ip router by str err: %s", err)
+	}
+
+	for k, v := range r.route.geo {
+		if k[0:1] == "!" {
+			if !strings.Contains(str, k[1:]) {
+				return v, nil
+			}
+		}
+		if strings.Contains(str, k) {
+			return v, nil
+		}
+	}
+
+	return store.GetSelectedProxy(), nil
+}
+
+func (*DefaultRouter) Name() string {
+	return "default-router"
+}
--- routing/router.go
+++ routing/router.go
@@ -1,142 +1,44 @@
 package routing
 
 import (
-	"bufio"
 	"fmt"
-	"github.com/spf13/viper"
-	"os"
 	"siu/tunnel/proxy"
-	"slices"
 	"sync"
 )
 
-type wildcard struct {
-	rule string
-	prx  proxy.Proxy
-}
-
-type routeTable struct {
-	exacts    map[string]proxy.Proxy
-	wildcards []*wildcard
-	area      map[string]proxy.Proxy
-}
-
-var (
-	table = &routeTable{}
-	rwx   sync.RWMutex
-)
-
-func AddRoute(host string, proxy proxy.Proxy) {
-	rwx.Lock()
-	defer rwx.Unlock()
-	if host[0] != '*' {
-		table.exacts[host] = proxy
-		return
-	}
-	w := &wildcard{
-		rule: host,
-		prx:  proxy,
-	}
-	table.wildcards = append(table.wildcards, w)
-}
-
-func RemoveRoute(host string) error {
-	rwx.Lock()
-	defer rwx.Unlock()
-
-	if _, ok := table.exacts[host]; ok {
-		delete(table.exacts, host)
-		return nil
-	}
-	slices.DeleteFunc(table.wildcards, func(w *wildcard) bool {
-		return w.rule == host
-	})
-
-	//config := util.CreateConfig("conf", "toml")
-	//filepath := util.ExpandHomePath(config, "route.table.path")
-	filepath := ""
-	rf, err := os.OpenFile(filepath, os.O_RDWR|os.O_TRUNC|os.O_APPEND, 0666)
-	if err != nil {
-		return err
-	}
-
-	bufW := bufio.NewWriter(rf)
-	for _, w := range table.wildcards {
-		if _, err = bufW.WriteString(fmt.Sprintf("%s|%s", w.rule, w.prx.GetName())); err != nil {
-			return err
-		}
-	}
-	if err = bufW.Flush(); err != nil {
-		return err
-	}
-
-	if err = rf.Close(); err != nil {
-		return err
-	}
-
-	return nil
-}
-
-func ListAllRoute() map[string]string {
-	rwx.RLock()
-	defer rwx.RUnlock()
-	var routes map[string]string
-	for k, v := range table.exacts {
-		routes[k] = v.GetName()
-	}
-	for _, w := range table.wildcards {
-		routes[w.rule] = w.prx.GetName()
-	}
-	return routes
+type Router interface {
+	Name() string
+	Route(string) (proxy.Proxy, error)
 }
 
 var (
 	router Router
-	rwxr   sync.RWMutex
+	rwx    sync.RWMutex
 )
 
-func InitRouter(v *viper.Viper) {
-	enable := v.GetBool("router.true")
-	if enable {
-		p := v.GetString("router.route.table.path")
-		router = NewDefaultRouter(p, p)
-
+func InitRouter(routePath, xdbPath string) {
+	var err error
+	router, err = CreateRouter(routePath, xdbPath)
+	if err != nil {
+		panic(fmt.Errorf("failed to initialize router: %w", err))
 	}
+}
+
+func CreateRouter(routePath, xdbPath string) (Router, error) {
+	rwx.Lock()
+	defer rwx.Unlock()
+	r, err := NewDefaultRouter(routePath, xdbPath)
+	return r, err
+}
+
+func R() Router {
+	rwx.RLock()
+	defer rwx.RUnlock()
+	return router
 }
 
 func CloseRouter() {
-	rwxr.Lock()
-	defer rwxr.Unlock()
+	rwx.Lock()
+	defer rwx.Unlock()
 	router = nil
-}
-
-func OpenRouter() {
-	rwxr.Lock()
-	defer rwxr.Unlock()
-}
-
-type Router interface {
-	Route(string) (proxy.Proxy, error)
-}
-
-func Route(host string, port uint16) (proxy.Proxy, string, error) {
-	if p, ok := table.exacts[host]; ok {
-		return p, "exacts", nil
-	}
-
-	for _, w := range table.wildcards {
-		if w.rule[1:] == host {
-			return w.prx, "wildcards", nil
-		}
-	}
-
-	if router == nil {
-		return proxy.GetDirect(), "none", nil
-	}
-
-	r, err := router.Route(host)
-	if err != nil {
-		return nil, "", err
-	}
-	return r, "ip", nil
 }
